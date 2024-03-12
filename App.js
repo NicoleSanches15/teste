5222e27b7341f2bd063d459da8b6cd92e3cc9092
NicoleSanches1 <nicolecarolinasanches016@gmail.com>
2024-03-12 12:15:59 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Nicole 12/03</Text>
+      <Button title='Salvar'></Button>
       <StatusBar style="auto" />
     </View>
   );
